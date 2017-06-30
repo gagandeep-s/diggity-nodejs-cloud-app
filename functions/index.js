293450ec10592e9732e0c7af6d7dbbfa6eed55e2
Ashish Nath Gupta <ashish@netision.com>
2017-06-30 21:17:45 +0530

    Test functions added for iOS Notification testing

--- functions/index.js
+++ functions/index.js
@@ -668,3 +668,67 @@ exports.handleSocialLogin = functions.https.onRequest((req, res) => {
 });
 // [END handleSocialLoginTrigger]
 // [END handleSocialLogin]
+
+
+
+
+
+// [START ashishSendNotification]
+exports.ashishSendNotification = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+console.log("ASHISH Request Data:", {
+    typeOfTokens: typeof (req.tokens),
+    tokens: req.tokens,
+    title : req.title,
+    body : req.body,
+    typeOfData: typeof (req.data),
+    data: req.data
+});
+/*
+        if (req && req.tokens && req.title && req.body) {
+            let payload = {
+                notification: {
+                    title: req.title, //iOS, Android, Web: The notification's title.
+                    body: req.body, //iOS, Android, Web: The notification's body text.
+                    // badge?: string, //???
+                    // clickAction: string, //???
+                    // color: string, //???
+                    // icon: string, //???
+                    // sound: string, //???
+                    // tag: string, //???
+                }
+            };
+            if (req.data && Object.keys(req.data).length > 0) {
+                payload.data = req.data; //The keys and values must both be strings. Keys can be any custom string, except for the following reserved strings: "from" & Anything starting with "google."
+            }
+
+            admin.messaging().sendToDevice(req.tokens, payload, {
+                contentAvailable: true, //On iOS, use this field to represent content-available in the APNs payload. When a notification or data message is sent and this is set to true, an inactive client app is awoken. On Android, data messages wake the app by default. On Chrome, this flag is currently not supported.
+                dryRun: false, //Whether or not the message should actually be sent. When set to true, allows developers to test a request without actually sending a message. When set to false, the message will be sent.
+                mutableContent: false, //On iOS, use this field to represent mutable-content in the APNs payload. When a notification is sent and this is set to true, the content of the notification can be modified before it is displayed, using a Notification Service app extension. On Android and Web, this parameter will be ignored.
+                priority: "high", //The priority of the message. Valid values are "normal" and "high". On iOS, these correspond to APNs priorities 5 and 10. By default, notification messages are sent with high priority, and data messages are sent with normal priority. Normal priority optimizes the client app's battery consumption and should be used unless immediate delivery is required. For messages with normal priority, the app may receive the message with unspecified delay. When a message is sent with high priority, it is sent immediately, and the app can wake a sleeping device and open a network connection to your server.
+                timeToLive: 2419200, //How long (in seconds) the message should be kept in FCM storage if the device is offline. The maximum time to live supported is four weeks, and the default value is also four weeks.  Keep in mind that a time_to_live value of 0 means messages that can't be delivered immediately are discarded.
+                // collapseKey: string, //String identifying a group of messages (for example, "Updates Available") that can be collapsed, so that only the last message gets sent when delivery can be resumed. This is used to avoid sending too many of the same messages when the device comes back online or becomes active. There is no guarantee of the order in which messages get sent. A maximum of four different collapse keys is allowed at any given time. This means an FCM connection server can simultaneously store four different send-to-sync messages per client app. If you exceed this number, there is no guarantee which four collapse keys the FCM connection server will keep.
+            }).then(response => {
+
+            }).catch(reason => {
+
+            });
+        }
+*/
+res.status(200).send();
+    });
+});
+// [END ashishSendNotification]
+
+// [START ashishConsoleLog]
+exports.ashishConsoleLog = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        if (req && req.query && req.query.ashishMessage && req.query.ashishValue) {
+            console.log(req.query.ashishMessage, JSON.parse(req.query.ashishValue));
+        }
+
+        res.status(200).send();
+    });
+});
+// [END ashishConsoleLog]
